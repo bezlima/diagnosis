f077aad9a2a539b48b40afb9a32ced76bd60bcb2
Lucas Lima <12400617-bezlima@users.noreply.gitlab.com>
2024-08-28 19:08:23 -0300

    feat: add client test init

---
+++ app/utils/cli_functions/start_client.py
@@ -0,0 +1,48 @@
+import click
+from app.db.db import SessionLocal
+from sqlalchemy import inspect
+from app.db.db import engine
+from app.models.client_model import Client
+from app.models.professional_model import Professional
+from app.services.client_service import create_client
+from app.schemas.client_schema import ClientCreate
+
+def start_client():
+
+    """Initialize Client"""
+    inspector = inspect(engine)
+    has_client_table = inspector.has_table('clients')
+    has_professional_table = inspector.has_table('professionals')
+
+    if has_professional_table & has_client_table:
+
+        db = SessionLocal()
+        has_professional = db.query(Professional.professional_id == 1).first()
+
+        if has_professional:
+            try:
+                if not db.query(Client).first():
+                    professinal = ClientCreate(
+                        
+                        name = "Diagnosis Client Example",
+                        email = "example.client@diagnosis.com",
+                        address = "SP / BR, 0000",
+                        age = 30,
+                        client_document_RG = "00000000000",
+                        client_document_CPF = "00000000000",
+                        professional_id = 1
+
+                    )
+
+                    create_client(db, professinal)
+                    click.echo("Client created successfully!")
+                
+                else:
+                    click.echo("Client already exists.")
+
+            finally:
+                db.close()
+
+        return click.echo("Professional id 1 does not exist.")
+    
+    return click.echo("Professional's or Client's table does not exist.")
--- app/utils/cli_functions/testing_diagnosis.py
+++ app/utils/cli_functions/testing_diagnosis.py
@@ -3,12 +3,22 @@ from .start_admin_role import start_admin_role
 from .start_database import start_db
 from .start_professional import start_professional
 from .start_server import start_server
+from .start_client import start_client
+from .start_report import start_report
 
 def testing_diagnosis():
     """Initialize Disgnosis testing"""
+
     click.echo(" Iniciando modelo de teste ... ")
 
     start_db()
+
     start_admin_role()
+
     start_professional()
+
+    start_client()
+
+    # start_report()
+
     start_server()
