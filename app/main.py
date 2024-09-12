9ba4b790c95b3892311beead8a039de5670604cb
Lucas Lima <12400617-bezlima@users.noreply.gitlab.com>
2024-09-12 14:40:10 -0300

    feat: updade app - add all features

--- app/main.py
+++ app/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
-from .models import professional_model, role_model, client_model, report_models
+from fastapi import FastAPI, Depends
 from .db.db import engine
+from .auth.dependencie.auth_dependencie import get_current_user
+
+from .models import professional_model, role_model, client_model, report_models, api_key_model
 
 from .routes.professional_route import professional_router
 from .routes.pdf_route import pdf_router
@@ -8,6 +10,9 @@ from .routes.client_route import client_router
 from .routes.role_route import role_router
 from .routes.login_routes import login_router
 from .routes.report_route import report_router
+
+from fastapi.middleware.cors import CORSMiddleware
+from .utils.veirfy_api_key import verify_api_key
 
 # Inicio do aplicativo
 app = FastAPI(
@@ -30,6 +35,7 @@ app = FastAPI(
         "url": "https://www.linkedin.com/in/bezlima/",
     },
     terms_of_service="https://api.suporte.com/termos",
+    dependencies=[Depends(verify_api_key)]
 )
 
 # inicio da base de dados
@@ -37,11 +43,27 @@ professional_model.Base.metadata.create_all(bind=engine)
 role_model.Base.metadata.create_all(bind=engine)
 client_model.Base.metadata.create_all(bind=engine)
 report_models.Base.metadata.create_all(bind=engine)
+api_key_model.Base.metadata.create_all(bind=engine)
 
 # Inclusão de rotas ao projeto
 app.include_router(login_router)
-app.include_router(role_router)
-app.include_router(professional_router)
-app.include_router(client_router)
-app.include_router(report_router)
-app.include_router(pdf_router)
+app.include_router(role_router, dependencies=[Depends(get_current_user)])
+app.include_router(professional_router, dependencies=[Depends(get_current_user)])
+app.include_router(client_router, dependencies=[Depends(get_current_user)])
+app.include_router(report_router, dependencies=[Depends(get_current_user)])
+app.include_router(pdf_router, dependencies=[Depends(get_current_user)])
+
+# Configurações de CORS
+origins = [
+    "http://localhost",
+    "http://localhost:8080",
+    "http://localhost:3000",
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,  
+    allow_credentials=True,  
+    allow_methods=["*"],  
+    allow_headers=["*"],  
+)
